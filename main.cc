da19659140ef419d11b6eb8c96b6fcfac2eb2fbd
calint <calin.tenitchi@gmail.com>
2023-01-31 18:58:59 +0100

    .

--- main.cc
+++ main.cc
@@ -7,6 +7,16 @@ extern "C" void tsk7();
 extern "C" void tsk8();
 extern "C" void tsk9();
 extern "C" void tsk10();
+extern "C" void tsk11();
+extern "C" void tsk12();
+extern "C" void tsk13();
+extern "C" void tsk14();
+extern "C" void tsk15();
+extern "C" void tsk16();
+extern "C" void tsk17();
+extern "C" void tsk18();
+extern "C" void tsk19();
+
 
 asm(".global tsk0,tsk1,tsk2,tsk3,tsk4");
 
@@ -43,12 +53,51 @@ asm("  hlt");
 asm("  jmp tsk4");
 
 extern "C" void tsk5(){
+//	static char sprite_clear[]{
+//			0,0,0,0,
+//			0,0,0,0,
+//			0,0,0,0,
+//			0,0,0,0,
+//	};
+	static char sprite_clear[]{
+			0,1,1,0,
+			1,1,1,1,
+			1,1,1,1,
+			0,1,1,0,
+	};
+	static char sprite_data[]{
+			0,2,2,0,
+			2,2,2,2,
+			2,2,2,2,
+			0,2,2,0,
+	};
+	Bitmap sprite{Addr(sprite_data),4,4};
+	Bitmap clear{Addr(sprite_clear),4,4};
+	Bitmap screen{Addr(0xa0000),320,200};
+	Coord x=24;
+	Coord x_prv=x;
+	while(true){
+//		sprite.to(screen,Coords{24,24});
+//		sprite.to(screen,Coords{32,24});
+//		sprite.to(screen,Coords{40,24});
+		clear.to(screen,Coords{x_prv,24});
+		sprite.to(screen,Coords{x,24});
+		x_prv=x;
+		x+=8;
+		if(x>80){
+			x=24;
+		}
+//		osca_yield();
+	}
+}
+
+extern "C" void tsk6(){
 	while(true){
 		osca_yield();
 		*(int*)0xa0154+=3;
 	}
 }
-extern "C" void tsk6(){
+extern "C" void tsk7(){
 	static char*p=(char*)0xa0400;
 	static char*nl=(char*)0xa0400;
 	static char key_prev=0;
@@ -74,7 +123,7 @@ extern "C" void tsk6(){
 	}
 }
 
-extern "C" void tsk7(){
+extern "C" void tsk8(){
 	while(true){
 		osca_yield();
 		int*p=(int*)0xa0080;
@@ -84,24 +133,6 @@ extern "C" void tsk7(){
 	}
 }
 
-extern "C" void tsk8(){
-	static char sprite_data[]{
-			0,2,2,0,
-			2,2,2,2,
-			2,2,2,2,
-			0,2,2,0,
-	};
-	Bitmap screen{Addr(0xa0000),320,200};
-//	Bitmap sprite{Addr(0xa0000+320*150),4,4};
-	Bitmap sprite{Addr(sprite_data),4,4};
-	while(true){
-		osca_yield();
-		sprite.to(screen,Coords{25,25});
-		sprite.to(screen,Coords{30,25});
-		sprite.to(screen,Coords{35,25});
-		//*(int*)0xa0480=osca_t;
-	}
-}
 
 extern "C" void tsk9(){
 	while(true){
@@ -119,6 +150,67 @@ extern "C" void tsk10(){
 		src.to(dst);
 	}
 }
+
+extern "C" void tsk11(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk12(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk13(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk14(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk15(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk16(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk17(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk18(){
+	while(true){
+		osca_yield();
+	}
+}
+
+extern "C" void tsk19(){
+	while(true){
+		osca_yield();
+	}
+}
+
+
+
+
+
+
+
 
 extern "C" void osca_keyb_ev(){
 	static char*p=(char*)0xa4000;
