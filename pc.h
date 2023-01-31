1a2da3ce983e14859999d0b77216802dde228632
calint <calin.tenitchi@gmail.com>
2023-01-31 23:10:23 +0100

    .

--- pc.h
+++ pc.h
@@ -1,5 +1,5 @@
-extern "C" void osca_keyb_ev();//called from keyboard interrupt when new keycode from keyboard
-extern "C" volatile const int osca_key;//last received keycode (char)
-extern "C" volatile const int osca_t;//time lower 32b
-extern "C" volatile const int osca_t1;//time higher 32b
+extern "C" void osca_keyb_ev();// called from keyboard interrupt when new keycode from keyboard
+extern "C" volatile const int osca_key;// last received keycode (char)
+extern "C" volatile const int osca_t;// time lower 32b
+extern "C" volatile const int osca_t1;// time higher 32b
 extern "C" inline void osca_yield(){asm("hlt");}
