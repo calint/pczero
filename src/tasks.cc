1fb49b3417a37378d36db24a860d60afd4759a9c
calint <calin.tenitchi@gmail.com>
2023-02-01 07:39:45 +0100

    .

--- src/tasks.cc
+++ src/tasks.cc
@@ -73,9 +73,9 @@ extern "C" void tsk5(){
 			2,2,2,2,
 			0,2,2,0,
 	};
-	static Bitmap sprite{Addr{sprite_data},4,4};
-	static Bitmap screen{Addr(0xa0000),320,200};
-	static Bitmap clear{Addr{sprite_clear},4,4};
+	Bitmap sprite{Addr{sprite_data},4,4};
+	Bitmap screen{Addr(0xa0000),320,200};
+	Bitmap clear{Addr{sprite_clear},4,4};
 	Coord x=24;
 	Coord x_prv=x;
 	while(true){
