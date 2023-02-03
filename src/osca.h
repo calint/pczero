adda39edd228a7d67dc50fd568f41a9569600881
calint <calin.tenitchi@gmail.com>
2023-02-03 13:44:38 +0100

    .

--- src/osca.h
+++ src/osca.h
@@ -1,9 +1,9 @@
 #pragma once
 
-// callback from _boot.S
+// callback from _osca.S
 extern "C" void osca_keyb_ev();// called from keyboard interrupt when new keycode from keyboard
 
-// symbols exported from _boot.S
+// symbols exported from _osca.S
 extern "C" volatile const unsigned char osca_key;// last received keycode (char)
 extern "C" volatile const int osca_t;// time lower 32b
 extern "C" volatile const int osca_t1;// time higher 32b
