fa75bf2fe3b50624a6562a07f57ac4900ff381f7
calint <calin.tenitchi@gmail.com>
2023-02-04 22:33:26 +0100

    .

--- src/main.cc
+++ src/main.cc
@@ -44,8 +44,30 @@ extern "C" void osca_keyb_ev(){
 extern "C" void tsk0(){
 	Vga13h dsp;
 	PrinterToBitmap pb{dsp.bmp()};
-	pb.pos(10,5);
-	pb.p('_');
+
+	pb.pos(1,30);
+	for(int i=0;i<16;i++){
+		pb.p_hex(i);
+	}
+	pb.fg(5).pos(2,10);
+	for(char i='0';i<='9';i++){
+		pb.p(i);
+	}
+	pb.fg(6);
+	for(char i='a';i<='z';i++){
+		pb.p(i);
+	}
+	pb.fg(7).pos(3,10+10);
+	for(char i='A';i<='Z';i++){
+		pb.p(i);
+	}
+
+	pb.fg(2).pos(4,15);
+	pb.p("hello world!").nl();
+	pb.fg(6).p("\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+	pb.fg(7).p(' ').p_hex_32b(sizeof(table_ascii_to_font)/sizeof(int));
+
+	pb.pos(10,5).fg(2).p('_');
 	while(true){
 		// handle keyboard events
 		while(true){
@@ -60,9 +82,11 @@ extern "C" void tsk0(){
 				pb.backspace().nl().p('_');
 				continue;
 			}
-			const char ch=table_scancode_to_ascii[sc];
+			char ch=table_scancode_to_ascii[sc];
 			if(!ch) // not an ascii. probably key release
 				continue;
+			if(ch>='a'&&ch<='z')
+				ch&=~32;
 			pb.backspace().p(ch).p('_');
 		}
 		osca_yield();
@@ -100,96 +124,101 @@ extern "C" void tsk3(){
 	}
 }
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-static unsigned char bmp0[]{
-	0,1,1,0,
-	1,1,1,1,
-	1,1,1,1,
-	0,1,1,0,
-};
-static unsigned char bmp1[]{
-	0,2,2,0,
-	2,2,2,2,
-	2,2,2,2,
-	0,2,2,0,
-};
-static unsigned char bmp2[]{
-	0,3,3,0,
-	3,3,3,3,
-	3,3,3,3,
-	0,3,3,0,
-};
-static unsigned char bmp3[]{
-	0,4,4,0,
-	4,4,4,4,
-	4,4,4,4,
-	0,4,4,0,
-};
 extern "C" void tsk4(){
-	static Vga13h dsp;
-	static const Bitmap&dbmp=dsp.bmp();
-	static PrinterToBitmap pb{dbmp};
-	static PrinterToBitmap pb2{dbmp};
-	static Bitmap bitmaps[]{
-		{Address{bmp0},DimensionPx{4,4}},
-		{Address{bmp1},DimensionPx{4,4}},
-		{Address{bmp2},DimensionPx{4,4}},
-		{Address{bmp3},DimensionPx{4,4}},
-	};
-	static Sprite sprites[]{
-		{bitmaps[0],Position{20.0f,25.f},Velocity{5,0.5f},Acceleration{0,0.5f}},
-		{bitmaps[1],Position{30.0f,25.f},Velocity{5,1.0f},Acceleration{0,0.5f}},
-		{bitmaps[2],Position{40.0f,25.f},Velocity{5,1.5f},Acceleration{0,0.5f}},
-		{bitmaps[3],Position{50.0f,25.f},Velocity{5,2.0f},Acceleration{0,0.5f}},
-	};
-
-	CoordPx x=24;
-	CoordPx x_prv=x;
-	pb.pos(1,30);
-	for(int i=0;i<16;i++){
-		pb.p_hex(i);
-	}
-	pb.pos(2,10).fg(5);
-	for(char i='0';i<='9';i++){
-		pb.p(i);
-	}
-	for(char i='a';i<='z';i++){
-		pb.p(i);
-	}
-	pb.p(' ');
-	pb.fg(6).p("hello world!").nl();
-	pb.p("\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-	pb.fg(7).p(' ').p_hex_32b(sizeof(table_ascii_to_font)/sizeof(int));
-
-	while(true){
-		bitmaps[0].to(dbmp,CoordsPx{x_prv,44});
-		bitmaps[1].to(dbmp,CoordsPx{x,44});
-		bitmaps[2].to(dbmp,CoordsPx{x,36});
-		bitmaps[3].to(dbmp,CoordsPx{x,28});
-		x_prv=x;
-		x+=8;
-		if(x>180){
-			x=24;
-		}
-		for(const auto&s:sprites){
-			s.to(dbmp);
-		}
-		for(auto&s:sprites){
-			s.update();
-		}
-		for(auto&s:sprites){
-			if(s.pos().y()>140){
-				Position p{s.pos().x(),140};
-				if(p.x()>static_cast<Coord>(dsp.bmp().dim().width())){
-					p.set_x(0);
-				}
-				s.set_pos(p);
-				Velocity v{s.velocity()};
-				v.set_y(-8);
-				s.set_velocity(v);
-			}
-		}
-		pb.fg(7).pos(5,10).transparent(true).p("transparent");
-//		osca_yield();
-	}
+	while(true)
+		osca_yield();
 }
+//
+//static unsigned char bmp0[]{
+//	0,1,1,0,
+//	1,1,1,1,
+//	1,1,1,1,
+//	0,1,1,0,
+//};
+//static unsigned char bmp1[]{
+//	0,2,2,0,
+//	2,2,2,2,
+//	2,2,2,2,
+//	0,2,2,0,
+//};
+//static unsigned char bmp2[]{
+//	0,3,3,0,
+//	3,3,3,3,
+//	3,3,3,3,
+//	0,3,3,0,
+//};
+//static unsigned char bmp3[]{
+//	0,4,4,0,
+//	4,4,4,4,
+//	4,4,4,4,
+//	0,4,4,0,
+//};
+//extern "C" void tsk4(){
+//	static Vga13h dsp;
+//	static const Bitmap&dbmp=dsp.bmp();
+//	static PrinterToBitmap pb{dbmp};
+//	static PrinterToBitmap pb2{dbmp};
+//	static Bitmap bitmaps[]{
+//		{Address{bmp0},DimensionPx{4,4}},
+//		{Address{bmp1},DimensionPx{4,4}},
+//		{Address{bmp2},DimensionPx{4,4}},
+//		{Address{bmp3},DimensionPx{4,4}},
+//	};
+//	static Sprite sprites[]{
+//		{bitmaps[0],Position{20.0f,25.f},Velocity{5,0.5f},Acceleration{0,0.5f}},
+//		{bitmaps[1],Position{30.0f,25.f},Velocity{5,1.0f},Acceleration{0,0.5f}},
+//		{bitmaps[2],Position{40.0f,25.f},Velocity{5,1.5f},Acceleration{0,0.5f}},
+//		{bitmaps[3],Position{50.0f,25.f},Velocity{5,2.0f},Acceleration{0,0.5f}},
+//	};
+//
+//	CoordPx x=24;
+//	CoordPx x_prv=x;
+//	pb.pos(1,30);
+//	for(int i=0;i<16;i++){
+//		pb.p_hex(i);
+//	}
+//	pb.pos(2,10).fg(5);
+//	for(char i='0';i<='9';i++){
+//		pb.p(i);
+//	}
+//	for(char i='a';i<='z';i++){
+//		pb.p(i);
+//	}
+//	pb.p(' ');
+//	pb.fg(6).p("hello world!").nl();
+//	pb.p("\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+//	pb.fg(7).p(' ').p_hex_32b(sizeof(table_ascii_to_font)/sizeof(int));
+//
+//	while(true){
+//		bitmaps[0].to(dbmp,CoordsPx{x_prv,44});
+//		bitmaps[1].to(dbmp,CoordsPx{x,44});
+//		bitmaps[2].to(dbmp,CoordsPx{x,36});
+//		bitmaps[3].to(dbmp,CoordsPx{x,28});
+//		x_prv=x;
+//		x+=8;
+//		if(x>180){
+//			x=24;
+//		}
+//		for(const auto&s:sprites){
+//			s.to(dbmp);
+//		}
+//		for(auto&s:sprites){
+//			s.update();
+//		}
+//		for(auto&s:sprites){
+//			if(s.pos().y()>140){
+//				Position p{s.pos().x(),140};
+//				if(p.x()>static_cast<Coord>(dsp.bmp().dim().width())){
+//					p.set_x(0);
+//				}
+//				s.set_pos(p);
+//				Velocity v{s.velocity()};
+//				v.set_y(-8);
+//				s.set_velocity(v);
+//			}
+//		}
+//		pb.fg(7).pos(5,10).transparent(true).p("transparent");
+////		osca_yield();
+//	}
+//}
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
