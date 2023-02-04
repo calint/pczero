5dd9b2c02c5974054962a693df081e36a7e867b1
calint <calin.tenitchi@gmail.com>
2023-02-04 12:16:21 +0100

    .

--- src/osca.h
+++ src/osca.h
@@ -4,9 +4,9 @@
 extern "C" void osca_keyb_ev();// called from keyboard interrupt when new keycode from keyboard
 
 // symbols exported from _osca.S
-extern "C" unsigned char osca_key;// last received keycode
-extern "C" unsigned int osca_t;// time lower 32b
-extern "C" unsigned int osca_t1;// time higher 32b
+extern "C" volatile const unsigned char osca_key;// last received keycode
+extern "C" volatile const unsigned int osca_t;// time lower 32b
+extern "C" volatile const unsigned int osca_t1;// time higher 32b
 
 // functions
 extern "C" inline void osca_yield(){asm("hlt");}// ? do task switch
