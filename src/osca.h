3093f17059f2667b90ff9090793f19b432b9bd68
calint <calin.tenitchi@gmail.com>
2023-02-16 20:34:47 +0100

    .

--- src/osca.h
+++ src/osca.h
@@ -1,18 +1,25 @@
 #pragma once
 
 // called from osca.S before starting tasks
-extern "C" void osca_init(); // called from keyboard interrupt when new scan code from keyboard
+extern "C" void osca_init();
 
+// called from keyboard interrupt when new scan code from keyboard
 // callback from osca.S:isr_kbd
-extern "C" void osca_keyb_ev(); // called from keyboard interrupt when new scan code from keyboard
+extern "C" void osca_keyb_ev();
 
 // symbols exported from osca.S
-extern "C" volatile const unsigned char osca_key; // last received keycode
-//extern "C" volatile const unsigned osca_t; // time lower 32b
-//extern "C" volatile const unsigned osca_th; // time higher 32b
-extern "C" volatile const unsigned osca_tmr_lo; // time lower 32b
-extern "C" volatile const unsigned osca_tmr_hi; // time higher 32b
 
-// functions
-extern "C" void osca_yield(); // switches to next task
+// last received keycode
+extern "C" volatile const unsigned char osca_key;
+
+// time lower 32b
+extern "C" volatile const unsigned osca_tmr_lo;
+
+// time higher 32b
+extern "C" volatile const unsigned osca_tmr_hi;
+
+// switches to next task
+extern "C" void osca_yield();
+
+// halts the system
 extern "C" inline void osca_halt(){asm("cli;hlt");}
