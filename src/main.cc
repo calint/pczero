443d52cb72269d1e2cbfe9c841541f938c88f0e3
calint <calin.tenitchi@gmail.com>
2024-03-08 22:38:04 +0100

    .

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,10 @@
 #include"libge.h"
 #include"kernel.h"
 
+using namespace osca;
+
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 extern "C" [[noreturn]] auto tsk0()->void{
-	using namespace osca;
 	osca_disable_interrupts();
 	const TaskId taskId=osca_active_task->get_id();
 	const Register eax=osca_active_task->eax;
@@ -100,7 +101,6 @@ asm("  incl 0xa0000+80");
 asm("  jmp tsk1");
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 extern "C" [[noreturn]] auto tsk2()->void{
-	using namespace osca;
 	while(true){
 		// copy kernel to screen
 		constexpr int kernel_size=512*2;
@@ -112,7 +112,6 @@ extern "C" [[noreturn]] auto tsk2()->void{
 }
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 extern "C" [[noreturn]] auto tsk3()->void{
-	using namespace osca;
 	osca_disable_interrupts();
 	const Register eax=osca_active_task->eax;
 	osca_enable_interrupts();
@@ -123,105 +122,8 @@ extern "C" [[noreturn]] auto tsk3()->void{
 		osca_yield();
 	}
 }
-
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 #include"game.h"
 extern "C" auto tsk4()->void{
 	osca::game::Game::run();
 }
-
-// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-//
-//static unsigned char bmp0[]{
-//	0,1,1,0,
-//	1,1,1,1,
-//	1,1,1,1,
-//	0,1,1,0,
-//};
-//static unsigned char bmp1[]{
-//	0,2,2,0,
-//	2,2,2,2,
-//	2,2,2,2,
-//	0,2,2,0,
-//};
-//static unsigned char bmp2[]{
-//	0,3,3,0,
-//	3,3,3,3,
-//	3,3,3,3,
-//	0,3,3,0,
-//};
-//static unsigned char bmp3[]{
-//	0,4,4,0,
-//	4,4,4,4,
-//	4,4,4,4,
-//	0,4,4,0,
-//};
-//extern "C" void tsk4(){
-//	static Vga13h dsp;
-//	static const Bitmap&dbmp=dsp.bmp();
-//	static PrinterToBitmap pb{dbmp};
-//	static PrinterToBitmap pb2{dbmp};
-//	static Bitmap bitmaps[]{
-//		{Address{bmp0},DimensionPx{4,4}},
-//		{Address{bmp1},DimensionPx{4,4}},
-//		{Address{bmp2},DimensionPx{4,4}},
-//		{Address{bmp3},DimensionPx{4,4}},
-//	};
-//	static Sprite sprites[]{
-//		{bitmaps[0],Position{20.0f,25.f},Velocity{5,0.5f},Acceleration{0,0.5f}},
-//		{bitmaps[1],Position{30.0f,25.f},Velocity{5,1.0f},Acceleration{0,0.5f}},
-//		{bitmaps[2],Position{40.0f,25.f},Velocity{5,1.5f},Acceleration{0,0.5f}},
-//		{bitmaps[3],Position{50.0f,25.f},Velocity{5,2.0f},Acceleration{0,0.5f}},
-//	};
-//
-//	CoordPx x=24;
-//	CoordPx x_prv=x;
-//	pb.pos(1,30);
-//	for(int i=0;i<16;i++){
-//		pb.p_hex(i);
-//	}
-//	pb.pos(2,10).fg(5);
-//	for(char i='0';i<='9';i++){
-//		pb.p(i);
-//	}
-//	for(char i='a';i<='z';i++){
-//		pb.p(i);
-//	}
-//	pb.p(' ');
-//	pb.fg(6).p("hello world!").nl();
-//	pb.p("\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-//	pb.fg(7).p(' ').p_hex_32b(sizeof(table_ascii_to_font)/sizeof(int));
-//
-//	while(true){
-//		bitmaps[0].to(dbmp,CoordsPx{x_prv,44});
-//		bitmaps[1].to(dbmp,CoordsPx{x,44});
-//		bitmaps[2].to(dbmp,CoordsPx{x,36});
-//		bitmaps[3].to(dbmp,CoordsPx{x,28});
-//		x_prv=x;
-//		x+=8;
-//		if(x>180){
-//			x=24;
-//		}
-//		for(const auto&s:sprites){
-//			s.to(dbmp);
-//		}
-//		for(auto&s:sprites){
-//			s.update();
-//		}
-//		for(auto&s:sprites){
-//			if(s.pos().y()>140){
-//				Position p{s.pos().x(),140};
-//				if(p.x()>static_cast<Coord>(dsp.bmp().dim().width())){
-//					p.set_x(0);
-//				}
-//				s.set_pos(p);
-//				Velocity v{s.velocity()};
-//				v.set_y(-8);
-//				s.set_velocity(v);
-//			}
-//		}
-//		pb.fg(7).pos(5,10).transparent(true).p("transparent");
-////		osca_yield();
-//	}
-//}
-// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
