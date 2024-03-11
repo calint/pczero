630c9d87638b2995c826eb66bfe29b49b19d7141
calint <calin.tenitchi@gmail.com>
2024-03-11 15:20:58 +0100

    .

--- src/kernel.h
+++ src/kernel.h
@@ -228,7 +228,7 @@ extern "C" auto osca_init()->void{
 	// start of contiguous free memory
 	Address free_mem_start=Address(&free_mem_start_symbol);
 	// size of free memory (to the vga address space that starts at 0xa'0000)
-	SizeBytes free_mem_size=0xa'0000-reinterpret_cast<SizeBytes>(free_mem_start);
+	SizeBytes free_mem_size=0x8'0000-reinterpret_cast<SizeBytes>(free_mem_start);
 	// clear free memory
 	pz_memset(free_mem_start,0,free_mem_size);
 
