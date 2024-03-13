464b3f21e40f34002afb4c1f838c5d99f27d0020
calint <calin.tenitchi@gmail.com>
2024-03-13 06:38:09 +0100

    .

--- src/libge.h
+++ src/libge.h
@@ -33,11 +33,11 @@ public:
 		normals=new Vector[indexes_size];
 		const PointIx n=indexes_size-1;
 		for(PointIx i=0;i<n;i++){
-			const Vector d{points[indexes[i+1]]-points[indexes[i]]};
-			normals[i]=d.normal().normalize();
+			const Vector v{points[indexes[i+1]]-points[indexes[i]]};
+			normals[i]=v.normal().normalize();
 		}
-		const Vector d{points[indexes[0]]-points[indexes[indexes_size-1]]};
-		normals[indexes_size-1]=d.normal().normalize();
+		const Vector v{points[indexes[0]]-points[indexes[indexes_size-1]]};
+		normals[indexes_size-1]=v.normal().normalize();
 	}
 };
 
