cd7a4b3285f48a722eac97c4e7805d6cd81a7673
calint <calin.tenitchi@gmail.com>
2024-03-23 15:20:02 +0100

    .

--- src/kernel.h
+++ src/kernel.h
@@ -323,8 +323,8 @@ extern "C" auto osca_on_key(const u8 scan_code)->void{
 }
 } // end namespace osca
 
-// called by C++ to allocate and free memory
-// note: inline not allowed by the c++ standard
+// called by c++ to allocate and free memory
+// note: inline not allowed by the standard
 void*operator new[](unsigned size){return osca::heap.alloc(size);}
 void*operator new(unsigned size){return osca::heap.alloc(size);}
 void operator delete(void*ptr)noexcept{osca::heap.free(ptr);}
